baad6bf11cd35a8dd7cef53ff94544904c535780
RayaLitova <raya.litova23@gmail.com>
2025-10-07 16:20:20 +0300

    Week 1 tasks

---
+++ Week1/tasks.hs
@@ -0,0 +1,31 @@
+-- succ
+add1 :: Int -> Int
+add1 _ = undefined
+
+-- mySum
+
+-- 1) pattern matching
+-- 2) if
+is5 :: Int -> Bool
+is5 _ = undefined
+
+fib :: Int -> Int
+fib _ = undefined
+
+isPrime :: Int -> Bool
+isPrime _ = undefined
+
+-- Game
+
+-- Actions
+-- 1 -> take damage
+-- 2 -> heal
+-- 3 -> take damage 2 times
+-- 4 -> take damage equal to the fifth number of fibonacci
+-- 5 -> if your health is below 5 : heal for 5, else : take 5 damage
+-- 6 -> if your health is a prime number take 20 damage, 
+        -- if your health is divisble by 2 : heal for 10, 
+        -- otherwise take damage equal to the smallest divisor
+-- sequence attack 
+
+
