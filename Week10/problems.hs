c8b504e4f3f62078f1dcf2952c795f0ef6de990a
RayaLitova <raya.litova23@gmail.com>
2025-12-16 15:25:05 +0200

    Week 10 problems

---
+++ Week10/problems.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE EmptyDataDeriving #-}
+-- cover all cases!
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+-- warn about incomplete patterns v2
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
+-- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+-- use different names!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
+-- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+-- На 23.12 няма да имаме упражнение
+
+-- generateAll от предния път
+
+-- Задача 1: Да се дефинират типове за неориентиран, ориентиран и тегловен граф
+
+-- Задачи с неориентирани графи:
+-- Задача 1: Да се напише функция, която връща списък от всички свързани компоненти в граф
+-- Задача 2: Да се напише функция, която проверява дали граф е Ойлеров
+
+-- Задачи с ориентирани графи:
+-- Задача 1: Да се напише функция, която приема граф и връща наредена тройка от върха, степен на входа и степен на изхода 
+-- Задача 2: Да се напише функция, която приема граф и обръща всички ребра
+-- Задача 3: Да се намерят всички цикли в граф
+
+-- Задачи с тегловни графи:
+-- Задача 1: Traveling salesman problem 
+
+
+
